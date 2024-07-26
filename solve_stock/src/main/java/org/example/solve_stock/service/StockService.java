c22cd16854f83c5237fd1d42e2a2e7b52b33df7c
Ferrorist <ferrorist@naver.com>
2024-07-26 21:25:12 +0900

    add: 동시성 문제 해결을 위한 synchronized 추가

--- solve_stock/src/main/java/org/example/solve_stock/service/StockService.java
+++ solve_stock/src/main/java/org/example/solve_stock/service/StockService.java
@@ -3,6 +3,7 @@ package org.example.solve_stock.service;
 import org.example.solve_stock.domain.Stock;
 import org.example.solve_stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -13,7 +14,16 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    public void decrease(Long id, Long quantity){
+    /*
+     * Transactional annotation
+     * 클래스를 래핑한 클래스로 새로 만들어서 실행하게 한다.
+     * 트랜잭션 종료 시점에 DB가 업데이트되는데,
+     * method가 완료된 후 실제 DB에 업데이트 되기 전에 다른 Thread가 호출할 수 있다.
+     * 그렇다면 그 Thread는 갱신 이전의 값으로 method를 수행한다.
+     */
+    // @Transactional
+    // synchronized : 해당 메소드는 한 개의 Thread만 접근이 가능하게 함.
+    public synchronized void decrease(Long id, Long quantity){
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
 
