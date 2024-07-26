7606fc9e6d50a8d09234f635d56f80c8f5101b56
Ferrorist <ferrorist@naver.com>
2024-07-26 18:59:55 +0900

    init: 프로젝트 생성

---
+++ solve_stock/src/test/java/org/example/solve_stock/SolveStockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.solve_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolveStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
